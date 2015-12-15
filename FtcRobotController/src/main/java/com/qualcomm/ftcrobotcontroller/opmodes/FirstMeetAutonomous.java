2b483bc38ec66f505c9668c8fa86b869e87e32ab
Mox <maxs.school.email@gmail.com>
2015-12-15 16:38:53 -0500

    oopsies

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FirstMeetAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FirstMeetAutonomous.java
@@ -71,7 +71,7 @@ public class FirstMeetAutonomous extends ResQ_Library {
 	public void moveTillLine() {
 		teamWeAreOn = getColor();
 		if(teamWeAreOn == Team.UNKNOWN) {
-			approach(-1);
+			approach(1);
 		}
 		else {
 			stopMoving();
