3048834d2ff7df142f49a45a12dcd437a804ab89
pyroninja8821 <pyroninja88211@gmail.com>
2016-03-08 18:15:12 -0500

    added wait for start

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/test/LinearDegreesTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/test/LinearDegreesTest.java
@@ -7,16 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  * Created by Admin on 3/8/2016.
  */
 public class LinearDegreesTest extends AutonomousLibrary {
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         initializeMapping();
         startIMU();
+
+        waitForStart();
         telemetry.addData("status", "turning");
         while(!driveTurnDegrees(270)) {
-            try {
-                waitForNextHardwareCycle();
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
+            waitForNextHardwareCycle();
         }
         telemetry.addData("status", "turned");
     }
